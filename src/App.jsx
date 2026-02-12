b02208f7c64b4051101b2746148aa85266505371
Yurii Pasternak <yuriipasternak@icloud.com>
2026-02-12 18:26:41 +0100

    fixet inports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import Header from "../src/components/Header/Header";
+import Header from "../src/components/Header/Header.jsx";
 import styles from "./app.module.css";
 import Hero from "./components/Hero/Hero";
-import Information from "./components/information/information";
+import Information from "./components/Information/Information.jsx";
 import DarkVeil from "./components/Hero/DarkVeil";
 import SpeakItalian from "./components/SpeakItalian/SpeakItalian";
 import Courses from "./components/Courses/Courses.jsx";
